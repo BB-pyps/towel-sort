f1078ab3784fcd8a8ef963d0375519b30d9aa87a
BB-pyps <80970001+BB-pyps@users.noreply.github.com>
2022-01-30 16:25:56 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix){
+    return [];
+  }
+  const argsArr = Array.from(matrix);
+  let newArr = [];
+  for (let i=0; i < argsArr.length; i++){
+    if(i % 2 === 0){
+    argsArr[i].sort((a, b) => (a - b));
+  }else{
+    argsArr[i].sort((a, b) => (b - a));
+  }
+  }
+  for (let i=0; i < argsArr.length; i++){
+    for (let j=0; j < argsArr[i].length; j++){
+      newArr.push(argsArr[i][j]);
+    } 
+  }
+  return newArr;
 }
